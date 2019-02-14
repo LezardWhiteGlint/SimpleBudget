94d2912e2f351456418759edcf7fe197aab42908
LezardWhiteGlint <30715385+LezardWhiteGlint@users.noreply.github.com>
2019-02-14 13:18:46 +0800

    Initial Commit

---
+++ SimpleBudget/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SimpleBudget
+//
+//  Created by Lezardvaleth on 2019/2/14.
+//  Copyright © 2019 Lezardvaleth. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SimpleBudget/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SimpleBudget
+//
+//  Created by Lezardvaleth on 2019/2/14.
+//  Copyright © 2019 Lezardvaleth. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
