64b91400eda1626892e5b6c24599e76f0af6655d
LezardWhiteGlint <30715385+LezardWhiteGlint@users.noreply.github.com>
2019-03-12 18:20:14 +0800

    add viewdidload function to make the spending and budget view reload when back button is pushed

--- SimpleBudget/SpendingAndBudgetViewController.swift
+++ SimpleBudget/SpendingAndBudgetViewController.swift
@@ -57,6 +57,12 @@ class SpendingAndBudgetViewController: UIViewController,UITableViewDelegate,UITa
         return cell
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        costs = loadCost()
+        budgets = loadBudget()
+        budgetTableView.reloadData()
+    }
+    
     //MARK: - UITabBarControllerDelegate
     
     func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
